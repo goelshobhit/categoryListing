ed24635430feb6a1f48705b99881c38c9f4dd57d
shobhit-pdgt <shobhit@primedigital.tech>
2020-08-28 21:44:50 +0530

    chore: fix navbar styles

--- app/components/NavBar/index.js
+++ app/components/NavBar/index.js
@@ -7,13 +7,11 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import ShoppingCardIcon from '@material-ui/icons/ShoppingCart';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -74,10 +72,10 @@ function ResponsiveDrawer(props) {
     <div className={classes.nav}>
       <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        {['Fruits', 'Pizza', 'Drink', 'Wines'].map(text => (
           <ListItem button key={text}>
             <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              <ShoppingCardIcon />
             </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
