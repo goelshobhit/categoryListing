09ebd833d0f7d31fe22d61c9e3d980f7c837f3cf
shobhit-pdgt <shobhit@primedigital.tech>
2020-09-01 08:39:06 +0530

    chore: add logger and update db.json

--- json-server/server.js
+++ json-server/server.js
@@ -1,10 +1,36 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
+const logger = require("./logger");
+const ngrok = require('ngrok');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+
+// get the intended host and port number, use localhost and port 3000 if not provided
+const customHost = process.env.HOST;
+const host = customHost || null; // Let http.Server use its default IPv6/4 host
+const prettyHost = customHost || 'localhost';
+
+// Start your app.
+server.listen(port, host, async (err) => {
+  if (err) {
+    return logger.error(err.message);
+  }
+
+  // Connect to ngrok in dev mode
+  if (ngrok) {
+    let url;
+    try {
+      url = await ngrok.connect(port);
+    } catch (e) {
+      return logger.error(e);
+    }
+    logger.appStarted(port, prettyHost, url);
+  } else {
+    logger.appStarted(port, prettyHost);
+  }
+});
